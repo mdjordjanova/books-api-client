3dcbd6e6d228862961c9039207783a28e0f8b537
maria <mariadjordjanova@marias-mbp.home>
2020-01-08 00:09:05 +0100

    add postresql data

---
+++ database/fixtures/init.sql
@@ -0,0 +1,10 @@
+insert into books.author (id, name) values (1,'John Ronald Reuel Tolkien');
+insert into books.author (id, name) values (2,'Franz Kafka');
+insert into books.author (id, name) values (3,'Stefan Zweig');
+
+insert into books.book (id, title, author_id, published, genre) values (1,'The Lord of the Rings', 1, '1954', 'fantasy');
+insert into books.book (id, title, author_id, published, genre) values (2,'The Hobbit', 1, '1937', 'fantasy');
+insert into books.book (id, title, author_id, published, genre) values (3,'The Silmarillion', 1, '1977', 'mythopoeia');
+insert into books.book (id, title, author_id, published, genre) values (4,'The Metamorphosis', 1, '1915', 'philosophical fiction');
+insert into books.book (id, title, author_id, published, genre) values (5,'The Trial', 1, '1925', 'philosophical fiction');
+insert into books.book (id, title, author_id, published, genre) values (6,'The Royal Game', 1, '1943', 'novella');
