3bcaeead88f35accc3de2b0f03ee7001992dbb03
maria <mariadjordjanova@marias-mbp.home>
2020-01-08 00:34:55 +0100

    add Author and Book to model

---
+++ src/main/java/com/books/model/Book.java
@@ -0,0 +1,76 @@
+package com.books.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(generator = "book_generator")
+    @SequenceGenerator(
+            name = "book_generator",
+            sequenceName = "book_sequence",
+            initialValue = 1000
+    )
+    private Long id;
+
+    @Column(columnDefinition = "title")
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Author author;
+
+    @Column(columnDefinition = "published")
+    private String published;
+
+    @Column(columnDefinition = "genre")
+    private String genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
